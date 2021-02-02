e2cbc3c9b3776140490124171a021ad22a181ef0
johnpoint <me@lvcshu.com>
2021-02-02 11:41:03 +0800

    fix :: 修复 ws 无法自动重连问题

--- src/apis/Websocket.go
+++ src/apis/Websocket.go
@@ -5,6 +5,7 @@
 package apis
 
 import (
+	"fmt"
 	"log"
 	"strings"
 	"time"
@@ -18,56 +19,74 @@ func websocketPush(url string, token string) {
 	} else {
 		url = "ws://" + strings.Split(url, "//")[1] + "/server/v2"
 	}
+
+	c := websocket.Conn{}
+	defer c.Close()
 	for {
-		log.Printf("connecting to %s", url)
+		fmt.Println("try")
+		done := make(chan int64)
+		c = websocket.Conn{}
+		log.Printf("[Websocket] connecting to %s", url)
 		c, _, err := websocket.DefaultDialer.Dial(url, nil)
 		if err != nil {
 			log.Println("[Websocket]", err)
 			time.Sleep(time.Duration(10) * time.Second)
 			continue
 		}
-		defer c.Close()
+
 		err = c.WriteMessage(websocket.TextMessage, []byte(token))
 		if err != nil {
 			log.Println("[Websocket] write:", err)
-			time.Sleep(time.Duration(10) * time.Second)
+			done <- 1
 			continue
 		}
 
-		done := make(chan struct{})
-
 		go func() {
-			defer close(done)
 			for {
-				_, message, err := c.ReadMessage()
-				if err != nil {
-					log.Println("read:", err)
-					time.Sleep(time.Duration(10) * time.Second)
-					continue
+				select {
+				case <-done:
+					done <- 1
+					return
+				default:
+					_, message, err := c.ReadMessage()
+					if err != nil {
+						log.Println("read:", err)
+						done <- 1
+						return
+					}
+					log.Printf("recv: %s", message)
 				}
-				log.Printf("recv: %s", message)
 			}
 		}()
 
 		status := make(chan string)
 		go func() {
-			defer close(done)
 			for {
-				status <- "pushStatus#" + infoMiniJSON()
-				time.Sleep(time.Duration(3) * time.Second)
+				select {
+				case <-done:
+					done <- 1
+					return
+				default:
+					status <- "pushStatus#" + infoMiniJSON()
+					time.Sleep(time.Duration(3) * time.Second)
+				}
 			}
 		}()
 
 		for {
 			select {
 			case <-done:
-				return
+				break
 			case t := <-status:
 				err := c.WriteMessage(websocket.TextMessage, []byte(t))
 				if err != nil {
 					log.Println("write:", err)
-					return
+					done <- 1
+					break
 				}
+			}
+			if <-done == 1 {
+				break
 			}
 		}
 	}
